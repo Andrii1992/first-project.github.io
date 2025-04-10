73a31975c610f8946d147aad9f1dfca915c1204c
Andrii1992 <andriihumeniak@gmail.com>
2025-04-10 16:10:10 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,21 @@
+import './App.css';
+
+function Glory(){
+    return (
+        <div>
+            <h1 className="country">GLORY TO UKRAINE!</h1>
+        </div>
+    );
+}
+
+export default function App(){
+    return (
+        <div>
+            <h1 className="ourCountry">СЛАВА УКРАЇНІ!</h1><br></br>
+            <Glory />
+        </div>
+    );
+}
+
+
+
